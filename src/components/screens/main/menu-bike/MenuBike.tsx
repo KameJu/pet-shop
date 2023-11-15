3a18bf150a07a79a104cfd669fbd328c88b59f84
KameJu <sasiskasssss6052@gmail.com>
2023-11-15 22:20:48 +0200

    Make some code rework

---
+++ src/components/screens/main/menu-bike/MenuBike.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import bike from "../../../../assets/images/bike_3.png";
+
+const MenuBike: FC = () => {
+  return (
+    <>
+      <img src={bike} alt="bike" />
+      <div className="text-yellow-300 absolute p-3 left-[3rem] top-[33.5rem]">
+        <div>Model: S-Works Tarmac SL7-Dura Ace Di2</div>
+        <div>Price: 2000$</div>
+        <div>Type: highway</div>
+      </div>
+    </>
+  );
+};
+
+export default MenuBike;
---
+++ src/components/screens/main/menu-nav/MenuNav.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+
+const MenuNav: FC = () => {
+  return (
+    <>
+      <div className="flex flex-col ml-14 mt-[20%]">
+        <span className="text-3xl font-bold">RIDE WITH US</span>
+        <span className="max-w-[360px] mt-10 text-xl">
+          Buy bicycles and accessories at Velocity stores, and also join the
+          Veloclub for a ride and good rest in the company of good people.
+        </span>
+        <div className="mt-8 flex gap-10">
+          <button className="btn-gray">TO CATALOG</button>
+          <button className="btn-yellow">JOIN THE CLUB</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MenuNav;
