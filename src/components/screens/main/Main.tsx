98e4a07b15cc1b6217c52c410922f9be0b63f5f4
KameJu <sasiskasssss6052@gmail.com>
2023-11-16 11:08:31 +0200

    Add Router

--- src/components/screens/main/Main.tsx
+++ src/components/screens/main/Main.tsx
@@ -3,17 +3,21 @@ import Row from "../../ui/grid/Row";
 import Column from "../../ui/grid/Column";
 import MenuBike from "./menu-bike/MenuBike";
 import MenuNav from "./menu-nav/MenuNav";
+import Nav from "../nav/Nav";
 
 const Main: FC = () => {
   return (
-    <Row>
-      <Column size={6} className="flex items-center">
-        <MenuNav />
-      </Column>
-      <Column size={6} className="relative">
-        <MenuBike />
-      </Column>
-    </Row>
+    <>
+      <Nav />
+      <Row>
+        <Column size={6} className="flex items-center">
+          <MenuNav />
+        </Column>
+        <Column size={6} className="relative">
+          <MenuBike />
+        </Column>
+      </Row>
+    </>
   );
 };
 
--- src/components/ui/main.tsx
+++ src/components/ui/main.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../screens/app/App";
 import "../../styles/global.css";
+import Router from "./Router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
